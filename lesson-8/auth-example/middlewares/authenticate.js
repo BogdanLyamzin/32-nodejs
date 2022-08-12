9d166643e64b038eccad24f20d8c2416cd224530
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-08-12 23:08:49 +0300

    add lesson

--- lesson-8/auth-example/middlewares/authenticate.js
+++ lesson-8/auth-example/middlewares/authenticate.js
@@ -1,9 +1,4 @@
-const {Unauthorized, NotFound} = require("http-errors");
-const jwt = require("jsonwebtoken");
 
-const {User} = require("../models");
-
-const {SECRET_KEY} = process.env;
 
 /*
 1. Извлекает заголовок authorization.
@@ -14,31 +9,3 @@ const {SECRET_KEY} = process.env;
 6. Прикрепляем его к запросу (объект req).
 7. Передаем обработку запроса дальше. 
 */
-
-const authenticate = async(req, res, next)=> {
-    try {
-        const [bearer, token] = req.headers.authorization.split(" ");
-        if(bearer !== "Bearer"){
-            throw new Unauthorized();
-        }
-        try {
-            const {id} = jwt.verify(token, SECRET_KEY);
-            const user = await User.findById(id);
-            if(!user) {
-                throw new NotFound("User not found");
-            }
-            if(!user.token) {
-                throw new Unauthorized();
-            }
-            req.user = user;
-            next();
-        } catch (error) {
-            throw new Unauthorized(error.message);
-        }
-    } catch (error) {
-        next(error);
-    }
-    
-}
-
-module.exports = authenticate;
